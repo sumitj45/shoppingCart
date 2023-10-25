7f7b4ba28e64d373904816625575c2ff0baa8091
Sumit Jaiswal <90706283+sumitj45@users.noreply.github.com>
2023-10-26 00:22:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,94 +7,94 @@ import ShowCourseComponent from './components/ShowCourseComponent';
 import UserCartComponent from './components/UserCartComponent';
 
 function App() {
-    const [products, setproducts] = useState([{
-            id: 1,
-            name: 'T-shirt',
-            price: 499,
-            image: 'https://i.ibb.co/kKdVZVQ/TSHIRT.jpg'
-        },
-        {
-            id: 2,
-            name: 'Jeans',
-            price: 1699,
-            image: 'https://i.ibb.co/3zPwm2S/jeans.webp'
-        },
-        {
-            id: 3,
-            name: 'Shoes',
-            price: 7999,
-            image: 'https://i.ibb.co/9HcxW5S/shoes.jpg'
-        }
-    ]);
+	const [products, setproducts] = useState([
+		{ id: 1, 
+		name: 'T-shirt', 
+		price: 499, 
+		image: 
+'https://i.ibb.co/kKdVZVQ/TSHIRT.jpg'
+		},
+		{ id: 2, 
+		name: 'Jeans', 
+		price: 1699, 
+		image: 
+'https://i.ibb.co/3zPwm2S/jeans.webp'
+		},
+		{ id: 3, 
+		name: 'Shoes', 
+		price: 7999, 
+		image: 
+'https://i.ibb.co/9HcxW5S/shoes.jpg'
+		}
+	]);
 
-    const [cartproducts, setCartproducts] = useState([]);
-    const [searchCourse, setSearchCourse] = useState('');
+	const [cartproducts, setCartproducts] = useState([]);
+	const [searchCourse, setSearchCourse] = useState('');
 
-    const addCourseToCartFunction = (GFGcourse) => {
-        const alreadyproducts = cartproducts
-            .find(item => item.product.id === GFGcourse.id);
-        if (alreadyproducts) {
-            const latestCartUpdate = cartproducts.map(item =>
-                item.product.id === GFGcourse.id ? {
-                    ...item,
-                    quantity: item.quantity + 1
-                } :
-                item
-            );
-            setCartproducts(latestCartUpdate);
-        } else {
-            setCartproducts([...cartproducts, { product: GFGcourse, quantity: 1 }]);
-        }
-    };
+	const addCourseToCartFunction = (course) => {
+		const alreadyproducts = cartproducts
+							.find(item => item.product.id === course.id);
+		if (alreadyproducts) {
+			const latestCartUpdate = cartproducts.map(item =>
+				item.product.id === course.id ? { 
+				...item, quantity: item.quantity + 1 } 
+				: item
+			);
+			setCartproducts(latestCartUpdate);
+		} else {
+			setCartproducts([...cartproducts, {product: course, quantity: 1}]);
+		}
+	};
 
-    const deleteCourseFromCartFunction = (GFGCourse) => {
-        const updatedCart = cartproducts
-            .filter(item => item.product.id !== GFGCourse.id);
-        setCartproducts(updatedCart);
-    };
+	const deleteCourseFromCartFunction = (course) => {
+		const updatedCart = cartproducts
+							.filter(item => item.product.id !== course.id);
+		setCartproducts(updatedCart);
+	};
 
-    const totalAmountCalculationFunction = () => {
-        return cartproducts
-            .reduce((total, item) =>
-                total + item.product.price * item.quantity, 0);
-    };
+	const totalAmountCalculationFunction = () => {
+		return cartproducts
+			.reduce((total, item) => 
+						total + item.product.price * item.quantity, 0);
+	};
 
-    const productsearchUserFunction = (event) => {
-        setSearchCourse(event.target.value);
-    };
+	const productsearchUserFunction = (event) => {
+		setSearchCourse(event.target.value);
+	};
 
-    const filterCourseFunction = products.filter((course) =>
-        course.name.toLowerCase().includes(searchCourse.toLowerCase())
-    );
+	const filterCourseFunction = products.filter((course) =>
+		course.name.toLowerCase().includes(searchCourse.toLowerCase())
+	);
 
-    return ( <
-        div className = "App" > { /* search component */ } {
-            /* <SearchComponent searchCourse={searchCourse} 
-            							productsearchUserFunction=
-            								{productsearchUserFunction} /> */
-        }
+	return (
+		<div className="App">
+      {/* search component */}
+			<SearchComponent searchCourse={searchCourse} 
+							productsearchUserFunction=
+								{productsearchUserFunction} />
 
 
-        <
-        main className = "App-main" >
+			<main className="App-main">
 
-        { /* showCourse Component */ } <
-        ShowCourseComponent products = { products }
-        filterCourseFunction = { filterCourseFunction }
-        addCourseToCartFunction = { addCourseToCartFunction }
-        />
+        {/* showCourse Component */}
+				<ShowCourseComponent
+					products={products}
+					filterCourseFunction={filterCourseFunction}
+					addCourseToCartFunction={addCourseToCartFunction}
+				/>
 
-        { /* userCartComponent */ } <
-        UserCartComponent cartproducts = { cartproducts }
-        deleteCourseFromCartFunction = { deleteCourseFromCartFunction }
-        totalAmountCalculationFunction = {
-            totalAmountCalculationFunction
-        }
-        setCartproducts = { setCartproducts }
-        /> <
-        /main> <
-        /div>
-    );
+{/* userCartComponent */}
+				<UserCartComponent
+					cartproducts={cartproducts}
+					deleteCourseFromCartFunction={deleteCourseFromCartFunction}
+					totalAmountCalculationFunction={
+						totalAmountCalculationFunction
+					}
+					setCartproducts={setCartproducts}
+				/>
+			</main>
+		</div>
+	);
 }
 
 export default App;
